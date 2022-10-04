075f628308e14e5484ddcff12db3d15dd6b89e42
joshburke <aphex756@gmail.com>
2022-10-04 15:09:36 -0400

    Lab 2 sys info

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-# A bash script to display basic syste info.
-hostname --fqdn
-hostnamectl
-hostname -I
-df -h ./
+# This is an improved script for displaying system information.
+
+# Fully qualified domain name information
+FQDN=$(hostname --fqdn)
+
+# Operating system information
+OS=$(hostnamectl | grep -wh 'Operating System')
+
+# IP address information
+IP=$(hostname -I)
+
+# Rootfile system free space information.
+SPACE=$(df -h | grep -w / | awk '{print $4}')
+
+# Running commands, display system information.
+echo 'Report for myvm'
+echo '=========================='
+echo 'FQDN:'  $FQDN
+echo 'Operating system and Version' $OS
+echo 'IP Address:' $IP
+echo 'Root filesystem free space:' $SPACE
+echo '=========================='
+
